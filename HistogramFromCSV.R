8fddcd7aba31bf5c2aa3d515539c3830d78ecc66
Sameesh <sameesh.fisat@gmail.com>
2016-07-14 02:12:05 -0400

    Create HistogramFromCSV.R

---
+++ HistogramFromCSV.R
@@ -0,0 +1,7 @@
+data=read.csv("marks.csv")
+print(data$mark)
+#data$mark -> mark is field name in csv file
+png("1.png")
+hist(data$mark,col="red",main="Histogram from a file marks.csv")
+print(mean(data$mark))
+dev.off()
